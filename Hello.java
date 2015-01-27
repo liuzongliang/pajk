0ab88dff60bcb44c3206d4444cca89db7e101434
liuzongliang <liuzongliang@users.noreply.github.com>
2015-01-27 09:22:06 +0800

    Create Hello.java
    写个文件

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello{
+  
+  public static String sayHello(){
+    return "Good Morning!";
+  }
+  
+  public static void main(String[] args){
+    
+    System.out.println("小明说"+sayHello());
+  }
+  
+}
